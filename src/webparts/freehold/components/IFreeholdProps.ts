658bdec6218c7c0b12e087639fdba238cc14e44e
Janani <jananisethuraman44@gmail.com>
2024-04-05 11:41:57 +0530

    initiale commit

---
+++ src/webparts/freehold/components/IFreeholdProps.ts
@@ -0,0 +1,7 @@
+export interface IFreeholdProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/freehold/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IFreeholdWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'FreeholdWebPartStrings' {
+  const strings: IFreeholdWebPartStrings;
+  export = strings;
+}
