0508c97ee71aacc629bd73e35008e51e16e81f64
Janani <jananisethuraman44@gmail.com>
2024-04-09 08:53:17 +0530

    Home page ui code

--- src/Common/Header/apiService.ts
+++ src/Common/Header/apiService.ts
@@ -1,25 +1,4 @@
-const fetchUserName = async () => {
-    try {
-        const response = await fetch("https://freeholddxb.sharepoint.com/_api/web/currentuser", {
-            headers: {
-                "Accept": "application/json;odata=verbose",
-                "Content-Type": "application/json;odata=verbose"
-            },
-            credentials: 'include' 
-        });
-        const data = await response.json();
-        return data.d.Title; 
-    } catch (error) {
-        console.error('Error fetching user name:', error);
-        return null;
-    }
-};
 
-export default fetchUserName;
-
-
-
-// not working
 
 // import { sp } from "@pnp/sp/presets/all"; 
 
