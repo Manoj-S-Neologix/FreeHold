7a341961c2f85ffb506600aeec51c3216dc3d781
balajivivek20 <166357191+balajivivek20@users.noreply.github.com>
2024-06-04 09:20:29 +0530

    Update ProjectUpload.tsx

--- src/webparts/freehold/components/ProjectUpload/ProjectUpload.tsx
+++ src/webparts/freehold/components/ProjectUpload/ProjectUpload.tsx
@@ -4,10 +4,12 @@ import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
+// import Button from '@mui/material/Button';
 import { Button, CircularProgress } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import { Box, Stack, Grid } from '@mui/material';
+// import DragAndDropUpload from '../../../../Common/DragAndDrop/DragAndDrop';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -20,42 +22,56 @@ import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import Checkbox from '@mui/material/Checkbox';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormHelperText from '@mui/material/FormHelperText';
 import styles from "./ProjectUpload.module.scss";
 import ProjectService from '../../Services/Business/ProjectService';
 import toast from 'react-hot-toast';
 import DropZone from '../../../../Common/DropZone/DropZone';
 import ClientService from "../../Services/Business/ClientService";
+//import CheckIcon from "@mui/icons-material/Check";
 import formatDate from "../../hooks/dateFormat";
 
 const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, selected, props, spContext, siteUrl, userRole }) => {
   const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-  const [, setFiles] = useState<File[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
   const [fileData, setFileData] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
   const [uploadFiles, setUploadFiles] = useState<any[]>([]);
   const [dropdownOptions, setDropdownOptions] = useState<any[]>([]);
+
   const [getClientDetails, setGetClientDetails] = useState<any[]>([]);
   const [getClient, setGetClient] = useState("");
+  //const [_, setCollectionOfDocuments] = React.useState<string[]>([]);
   const [deleteId, setDeleteId] = useState<number>(0);
+
+  console.log(getClient, getClientDetails, particularClientAllData, "getClientgetClient");
   const { control, handleSubmit, formState: { errors }, setValue, getValues, reset } = useForm();
   const [isUnitDocumentChecked, setIsUnitDocumentChecked] = useState(false);
 
   const handleFileInput = (selectedFiles: File[]) => {
+    console.log(selectedFiles, "selectedFiles");
     setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
   };
 
   const handleCloseDeleteDialog = () => {
     setIsDeleteDialogOpen(false);
+    // fetchData();
   };
 
   const handleCancel = () => {
     onClose();
   };
 
+  console.log(files, "files");
+
+  console.log(setFileData, "setFileData");
+
   const clientService = ClientService();
   const clientListName = "Client_Informations";
   const selectQuery = "Id,Title,ClientLibraryGUID,AssignedStaff/Title,AssignedStaff/EMail";
   const expand = 'AssignedStaff';
+  //alert("userROle | " + userRole);
   const filter = (userRole === "staff") ? `AssignedStaff/EMail eq '${spContext.pageContext.user.email}'` : "";
 
   const apiCall = async () => {
@@ -64,6 +80,7 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
       if (data) {
         const assignClientIds = particularClientAllData[0].assignClientId.split(',').map((id: any) => Number(id.trim()));
         const filteredData = data.filter(item => assignClientIds.includes(item.Id));
+        console.log(filteredData, "filteredData");
         const mappedData = filteredData.map(item => ({
           id: item.Id,
           name: item.Title,
@@ -80,13 +97,20 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
   const [isLoading, setIsLoading] = useState(true);
   const [getFoldersResponse, setGetFoldersResponse] = useState<any[]>([]);
   const getProjectCode = particularClientAllData[0]?.projectNumber;
+  //const getProjectId = particularClientAllData[0]?.Id;
   const getProjectUrl = particularClientAllData[0]?.webURL;
 
   const getDocumentsFromFolder = async (libraryGuid: string) => {
     try {
+      //const results: any = await ProjectService().getDocumentsFromFolder(libraryGuid);
+      //console.log(results, 'guidresult')
       const getLibraryName = getClientDetails.filter((item: any) => item.libraryGUID === libraryGuid)[0].name;
+      console.log(`${getProjectCode}/${getLibraryName}`, 'getProjectName/getLibraryName');
       const getFolders: any = await ProjectService().getAllFoldersInLibrary(`${getProjectUrl}/${getLibraryName}`);
+      //console.log('Retrieved files:', results,);
+      console.log('getFolders', getFolders);
       setGetFoldersResponse(getFolders);
+
     } catch (error) {
       console.error('Error fetching documents:', error);
     }
@@ -98,11 +122,13 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
 
   const getDocumentsLibPath = async (type: string) => {
 
-    const projectRelativePath = (type === "project") ? `${getProjectUrl}` : (type === "client") ? `${getProjectUrl}/${getValues("clientName")}` : `${getProjectUrl}/${getValues("clientName")}/${getValues("unitDocument")}`;
+    let projectRelativePath = (type === "project") ? `${getProjectUrl}` : (type === "client") ? `${getProjectUrl}/${getValues("clientName")}` : `${getProjectUrl}/${getValues("clientName")}/${getValues("unitDocument")}`;
 
     try {
       const results: any = await ProjectService().getFolderItems(spContext, siteUrl, `${projectRelativePath}`, getProjectCode);
+      console.log(results, "File Datas");
       setFileData(results);
+
       setIsLoading(false);
     } catch (error) {
       setIsLoading(false);
@@ -114,6 +140,7 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
     const projectService = ProjectService();
     projectService.getProject('project Checklist')
       .then((results) => {
+        console.log(results, 'client');
         if (results) {
           setDropdownOptions(results);
         }
@@ -122,6 +149,21 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
         console.error('Error fetching SharePoint data:', error);
       });
   };
+
+  const fileInfoArray = uploadFiles?.map((file: any) => ({
+    lastModified: file.lastModified,
+    lastModifiedDate: file.lastModifiedDate,
+    name: file.name,
+    size: file.size,
+    type: file.type,
+    webkitRelativePath: file.webkitRelativePath
+  }));
+
+  console.log(fileInfoArray, 'fileInfoArray');
+
+  console.log(getClientDetails, "uploadFilesgetGuid");
+
+  console.log(particularClientAllData, 'getProjectName..');
 
   React.useEffect(() => {
     if (uploadFiles && uploadFiles.length > 0) {
@@ -134,21 +176,33 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
   }, []);
 
   const onDelete = (index: number) => {
+
+    //console.log("getvalues", getValues());
+
     setUploadFiles(prevFiles => prevFiles.filter((_, i) => i !== index));
+
     const clientName: string = getValues("clientName");
+    const unitDocumentName: string = getValues("unitDocument");
     reset();
+
     setValue("clientName", clientName);
+    setValue("unitDocument", unitDocumentName);
+
+    //const upFiles:any[] = uploadFiles;
+
     uploadFiles.filter((_, i) => i !== index).map((uploadedFile, index) => (
       setValue(`projectChecklist-${index}`, uploadedFile.checklist)
     ));
+
+    //console.log("getvalues", getValues());
   };
 
-
+  //project upload with meta data
   const handleSave = handleSubmit(async (data: any, libraryGuid: any) => {
     setLoading(true);
 
     if (getClient !== "") {
-      const clientInfo: any = (particularClientAllData[0].clientDetails).filter((o: any) => o.Name === getClient);
+      const clientInfo: any = (particularClientAllData[0].clientDetails).filter((o: any) => o.Name == getClient);
 
       const updatedData = {
         DMSProject: particularClientAllData[0].projectName,
@@ -157,16 +211,22 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
         DMSClientID: (clientInfo[0].Id).toString(),
         DMSUnit: (data.unitDocument !== '') ? data.unitDocument : "",
       }
+
+      //console.log("clientInfo", clientInfo);
+
       try {
         const apiResponse = ProjectService();
+        console.log(data, 'projectdata..')
         const folderUrl = `${particularClientAllData[0].webURL}/${getClient}`;
         if (data.unitDocument !== '' && isUnitDocumentChecked) {
 
           const folderUrl = `${particularClientAllData[0].webURL}/${getClient}/${data.unitDocument}`
+          console.log(folderUrl, 'projectfolderurl..')
           await apiResponse.addDocumentsToFolder(folderUrl, uploadFiles);
           await apiResponse.updateProjectDocumentMetadata(folderUrl, uploadFiles, updatedData)
         }
         else {
+          // Upload documents to the specified client's folder
           await apiResponse.updateProjectDocumentMetadata(folderUrl, uploadFiles, updatedData)
           await apiResponse.addDocumentsToFolder(folderUrl, uploadFiles);
         }
@@ -188,9 +248,11 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
 
       } catch (error) {
         setLoading(false);
+        console.error("Failed to add client and document:", error);
         toast.error(`Failed to add client and document: ${error}`);
       }
     } else {
+
       const updatedData = {
         DMSProject: particularClientAllData[0].projectName,
         DMSProjectID: (particularClientAllData[0].Id).toString(),
@@ -198,16 +260,22 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
         DMSClientID: "",
         DMSUnit: "",
       }
+
+      //console.log("clientInfo", clientInfo);
+
       try {
         const apiResponse = ProjectService();
+        console.log(data, 'projectdata..')
         const folderUrl = `${particularClientAllData[0].webURL}/${getClient}`;
         if (data.unitDocument !== '' && isUnitDocumentChecked) {
 
           const folderUrl = `${particularClientAllData[0].webURL}/${getClient}/${data.unitDocument}`
+          console.log(folderUrl, 'projectfolderurl..')
           await apiResponse.addDocumentsToFolder(folderUrl, uploadFiles);
           await apiResponse.updateProjectDocumentMetadata(folderUrl, uploadFiles, updatedData)
         }
         else {
+          // Upload documents to the specified client's folder
           await apiResponse.updateProjectDocumentMetadata(folderUrl, uploadFiles, updatedData)
           await apiResponse.addDocumentsToFolder(folderUrl, uploadFiles);
         }
@@ -229,17 +297,23 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
 
       } catch (error) {
         setLoading(false);
+        console.error("Failed to add client and document:", error);
         toast.error(`Failed to add client and document: ${error}`);
       }
     }
+
   });
 
   const handleDelete = (getGuid: any) => {
+
     const apiResponse = ProjectService();
     apiResponse.deleteFile(particularClientAllData[0].GUID, deleteId)
       .then(() => {
         setIsDeleteDialogOpen(false);
+        console.log("File deleted successfully!");
         toast.success('File deleted successfully!');
+        //fetchData(particularClientAllData[0].GUID);
+
         if (isUnitDocumentChecked) {
           getDocumentsLibPath("unit");
         } else {
@@ -256,6 +330,7 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
     <Box sx={{ width: '100', padding: '20px' }}>
       <Stack direction="column" spacing={2}>
         <Dialog open={open} onClose={onClose}>
+
           <DialogTitle>
             <div className="d-flex flex-column">
               <div className="d-flex justify-content-between
@@ -283,13 +358,18 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
           </IconButton>
           <DialogContent>
             <Stack direction="column" spacing={2}>
+              {/* <form onSubmit={handleSubmit}> */}
+
+              {/* Document selection section */}
               <Stack direction="column" gap={3}>
                 <Grid container spacing={2}>
+                  {/* Client grid */}
                   <Grid item sm={6}>
                     <Controller
                       name="clientName"
                       control={control}
                       defaultValue=""
+                      //rules={{ required: 'Client Name is required' }}
                       render={({ field }) => (
                         <>
                           <InputLabel htmlFor="client-name">Client Name</InputLabel>
@@ -305,12 +385,21 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                             error={!!errors.clientName}
                             helperText={errors?.clientName?.message}
                             onChange={(e: any) => {
+                              console.log(e.target.value);
                               setGetClient(e.target.value);
                               setIsUnitDocumentChecked(false);
+                              console.log("particularClientAllData: ", particularClientAllData);
                               const getLibraryName = getClientDetails.filter((item: any) => item.name === e.target.value)[0].libraryGUID
+
+                              console.log(getLibraryName, "getLibraryName");
                               setValue('clientName', e.target.value);
                               setGetGuid(e.target.value);
+
+                              //Populate Unit documents folder
                               getDocumentsFromFolder(getLibraryName);
+
+                              //Get documents for client selection
+                              //fetchData(getLibraryName);
                               getDocumentsLibPath("client");
                             }}
                           >
@@ -325,6 +414,7 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                     />
                   </Grid>
 
+                  {/* Unit document grid */}
                   <Grid item sm={6}>
                     <Stack direction="row" alignItems="center">
                       <Checkbox
@@ -335,6 +425,7 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                           if (!e.target.checked) {
                             getDocumentsLibPath("client");
                           }
+
                         }}
                         size="small"
                         sx={{ p: 0, mr: 2 }}
@@ -356,8 +447,18 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                           placeholder="Select Unit..."
                           size="small"
                           onChange={(e: any) => {
+                            console.log(e.target.value);
                             setValue('unitDocument', e.target.value);
+
                             getDocumentsLibPath("unit");
+
+                            /* const getLibraryName = getClientDetails.filter((item: any) => item.name === getGuid)[0].libraryGUID
+   
+                            const libraryPath = `${getLibraryName}/${e.target.value}`;
+   
+                            fetchData(libraryPath) */
+                            //Get documents for client selection
+                            //fetchData(getLibraryName);
                           }}
                           required
                         >
@@ -371,10 +472,13 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                       )}
                     />
                   </Grid>
+
                 </Grid>
               </Stack>
 
+              {/* Dropzone */}
               <Box>
+
                 <DropZone
                   onFilesAdded={handleFileInput}
                   files={uploadFiles}
@@ -382,8 +486,10 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                 />
               </Box>
 
+              {/*Dropzone selected document display */}
               {uploadFiles.length > 0 && dropdownOptions.length > 0 && (
                 <>
+
                   <TableContainer>
                     <Table>
                       <TableHead>
@@ -410,14 +516,16 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                                     variant="outlined"
                                     select
                                     size="small"
+                                    //value={uploadedFile.checklist}
                                     required
                                     error={!!errors[`projectChecklist-${index}`]}
                                     helperText={errors[`projectChecklist-${index}`]?.message}
-                                    style={{ width: 200 }} 
+                                    style={{ width: 200 }} // Fixed width
                                     onChange={(e: any) => {
                                       field.onChange(e);
                                       const newValue = e.target.value;
                                       setValue('projectChecklist', e.target.value);
+                                      console.log(newValue, 'e.target')
                                       setUploadFiles(prevFiles => {
                                         const updatedFiles = [...prevFiles];
                                         updatedFiles[index].checklist = newValue;
@@ -470,6 +578,8 @@ const ViewUpload: React.FC<any> = ({ open, onClose, particularClientAllData, sel
                   </DialogActions>
                 </>
               )}
+
+              {/* Document display */}
               {
                 < TableContainer >
                   <Table>
